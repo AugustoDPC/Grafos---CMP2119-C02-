dfbdfd2985902ff5f46275fb907be115cce5e034
AugustoDPC <142420017+AugustoDPC@users.noreply.github.com>
2026-04-28 22:58:35 +0000

    erro arrumado

--- MainS.java
+++ MainS.java
@@ -22,24 +22,29 @@ public class MainS {
 
         ListaAdjacencia[] lista = new ListaAdjacencia[vertices];
 
-        // Entrada da lista de adjacência
         for (int i = 0; i < vertices; i++) {
             lista[i] = new ListaAdjacencia();
 
             System.out.print("\nQuantos vizinhos tem o vertice " + i + "? ");
             lista[i].quantidade = sc.nextInt();
 
-            if (lista[i].quantidade > 0) {
-                lista[i].vizinhos = new int[lista[i].quantidade];
+            while (lista[i].quantidade < 0 || lista[i].quantidade >= vertices) {
+                System.out.print("Quantidade invalida. Digite novamente: ");
+                lista[i].quantidade = sc.nextInt();
+            }   
 
-                System.out.println("Digite os vizinhos do vertice " + i + ":");
+            lista[i].vizinhos = new int[lista[i].quantidade];
 
-                for (int j = 0; j < lista[i].quantidade; j++) {
+            System.out.println("Digite os vizinhos do vertice " + i + ":");
+
+            for (int j = 0; j < lista[i].quantidade; j++) {
+                lista[i].vizinhos[j] = sc.nextInt();
+                while (lista[i].vizinhos[j] < 0 || lista[i].vizinhos[j] >= vertices) {
+                    System.out.print("Vizinho invalido. Digite novamente: ");
                     lista[i].vizinhos[j] = sc.nextInt();
                 }
-            } else {
-                lista[i].vizinhos = new int[0];
             }
+
         }
 
         int verticeInicial;
@@ -52,54 +57,42 @@ public class MainS {
             verticeInicial = sc.nextInt();
         }
 
-        // Vetor para marcar os vértices visitados
         boolean[] visitado = new boolean[vertices];
 
-        // Vetor para armazenar a distância em número de arestas
         int[] distancia = new int[vertices];
 
         for (int i = 0; i < vertices; i++) {
             distancia[i] = -1;
         }
 
-        // Fila manual usando vetor
         int[] fila = new int[vertices];
         int inicioFila = 0;
         int fimFila = 0;
 
-        // Coloca o vértice inicial na fila
         fila[fimFila] = verticeInicial;
         fimFila++;
 
-        // Marca o vértice inicial como visitado
         visitado[verticeInicial] = true;
 
-        // A distância do vértice inicial para ele mesmo é 0
         distancia[verticeInicial] = 0;
 
         System.out.print("\nOrdem de visita da BFS (Em niveis): ");
 
-        // Enquanto a fila não estiver vazia
         while (inicioFila < fimFila) {
 
-            // Remove o primeiro elemento da fila
             int atual = fila[inicioFila];
             inicioFila++;
 
             System.out.print(atual + " ");
 
-            // Percorre todos os vizinhos do vértice atual
             for (int j = 0; j < lista[atual].quantidade; j++) {
                 int vizinho = lista[atual].vizinhos[j];
 
-                // Se o vizinho ainda não foi visitado
                 if (!visitado[vizinho]) {
                     visitado[vizinho] = true;
 
-                    // Calcula a distância do vizinho
                     distancia[vizinho] = distancia[atual] + 1;
 
-                    // Coloca o vizinho no fim da fila
                     fila[fimFila] = vizinho;
                     fimFila++;
                 }
