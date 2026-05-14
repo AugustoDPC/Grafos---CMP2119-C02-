5803132df8fe78d95381d77e2a9a6686d308a92c
AugustoDPC <142420017+AugustoDPC@users.noreply.github.com>
2026-05-14 14:20:57 +0000

    verifique se ele é conexo utilizando a multiplicação das matrizes.

---
+++ Main.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int quantidadeVertices;
+
+        System.out.print("Digite a quantidade de vertices: ");
+        quantidadeVertices = sc.nextInt();
+
+        while (quantidadeVertices <= 0) {
+            System.out.print("Quantidade invalida. Digite novamente: ");
+            quantidadeVertices = sc.nextInt();
+        }
+
+        int[][] matrizAdjacencia = new int[quantidadeVertices][quantidadeVertices];
+        int[][] matrizMultiplicadora = new int[quantidadeVertices][quantidadeVertices];
+        int[][] matrizConexo = new int[quantidadeVertices][quantidadeVertices];
+
+        System.out.println("\nDigite a matriz de adjacencia:");
+
+        for (int i = 0; i < quantidadeVertices; i++) {
+            for (int j = 0; j < quantidadeVertices; j++) {
+                matrizAdjacencia[i][j] = sc.nextInt();
+
+                while (matrizAdjacencia[i][j] != 0 && matrizAdjacencia[i][j] != 1) {
+                    System.out.print("Valor invalido. Digite apenas 0 ou 1: ");
+                    matrizAdjacencia[i][j] = sc.nextInt();
+                }
+
+                matrizMultiplicadora[i][j] = matrizAdjacencia[i][j];
+                matrizConexo[i][j] = matrizAdjacencia[i][j];
+            }
+        }
+
+        System.out.println("\nA1:");
+        mostrarMatriz(matrizMultiplicadora, quantidadeVertices);
+
+        for (int comprimento = 2; comprimento <= quantidadeVertices - 1; comprimento++) {
+            int[][] matrizResultado = new int[quantidadeVertices][quantidadeVertices];
+
+            for (int i = 0; i < quantidadeVertices; i++) {
+                for (int j = 0; j < quantidadeVertices; j++) {
+                    matrizResultado[i][j] = 0;
+
+                    for (int k = 0; k < quantidadeVertices; k++) {
+                        matrizResultado[i][j] += matrizMultiplicadora[i][k] * matrizAdjacencia[k][j];
+                    }
+                }
+            }
+
+            matrizMultiplicadora = matrizResultado;
+
+            for (int i = 0; i < quantidadeVertices; i++) {
+                for (int j = 0; j < quantidadeVertices; j++) {
+                    matrizConexo[i][j] += matrizMultiplicadora[i][j];
+                }
+            }
+
+            System.out.println("\nA" + comprimento + ":");
+            mostrarMatriz(matrizMultiplicadora, quantidadeVertices);
+        }
+
+        System.out.println("\nMatriz acumuladora para ver se é conexo ou  nao:");
+        mostrarMatriz(matrizConexo, quantidadeVertices);
+
+        boolean grafoConexo = true;
+
+        for (int i = 0; i < quantidadeVertices; i++) {
+            for (int j = 0; j < quantidadeVertices; j++) {
+                if (i != j && matrizConexo[i][j] == 0) {
+                    grafoConexo = false;
+                }
+            }
+        }
+
+        if (grafoConexo) {
+            System.out.println("\nO grafo eh conexo.");
+        } else {
+            System.out.println("\nO grafo eh desconexo.");
+        }
+
+        sc.close();
+    }
+
+    public static void mostrarMatriz(int[][] matriz, int quantidadeVertices) {
+        for (int i = 0; i < quantidadeVertices; i++) {
+            for (int j = 0; j < quantidadeVertices; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- exercicio12.cpp
+++ exercicio12.cpp
@@ -22,60 +22,46 @@ int main() {
         }
     }
 
+    // Valida se e grafo simples
     for (int i = 0; i < n; i++) {
         if (grafo[i][i] != 0) {
-            cout << "\nO grafo nao eh simples, pois possui laco.\n";
+            cout << "\nO grafo nao e simples, pois possui laco.\n";
             return 0;
         }
 
         for (int j = 0; j < n; j++) {
             if (grafo[i][j] != grafo[j][i]) {
-                cout << "\nO grafo nao eh simples, pois a matriz nao eh simetrica.\n";
+                cout << "\nO grafo nao e simples, pois a matriz nao e simetrica.\n";
                 return 0;
             }
         }
     }
 
-    int qtdSubgrafo;
+    bool achouClique = false;
 
-    cout << "\n Digite a quantidade de vertices que o subgrafo tem: ";
-    cin >> qtdSubgrafo;
+    // Testa todos os caminhos/subgrafos de 3 vertices
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
 
-    int subgrafo[qtdSubgrafo];
+                if (grafo[i][j] == 1 &&
+                    grafo[i][k] == 1 &&
+                    grafo[j][k] == 1) {
 
-    while (qtdSubgrafo < 3 || qtdSubgrafo > n) {
-            cout << "Vertice invalido. Digite novamente: ";
-            cin >> qtdSubgrafo;
-    }
+                    cout << "\nO grafo possui clique de tamanho 3.\n";
+                    cout << "Clique encontrado: "
+                         << i + 1 << " "
+                         << j + 1 << " "
+                         << k + 1 << "\n";
 
-    cout << "Digite os vertices do subgrafo:\n";
-
-    for (int i = 0; i < qtdSubgrafo; i++) {
-        cin >> subgrafo[i];
-
-        while (subgrafo[i] < 0 || subgrafo[i] > n) {
-            cout << "Vertice invalido. Digite novamente: ";
-            cin >> subgrafo[i];
-        }
-    }
-
-    bool clique = true;
-
-    for (int i = 0; i < qtdSubgrafo; i++) {
-        for (int j = i + 1; j < qtdSubgrafo; j++) {
-            int v1 = subgrafo[i];
-            int v2 = subgrafo[j];
-
-            if (grafo[v1][v2] != 1) {
-                clique = false;
+                    achouClique = true;
+                }
             }
         }
     }
 
-    if (clique) {
-        cout << "\nO subgrafo informado eh um clique.\n";
-    } else {
-        cout << "\nO subgrafo informado nao eh um clique.\n";
+    if (!achouClique) {
+        cout << "\nO grafo nao possui clique de tamanho 3.\n";
     }
 
     return 0;
